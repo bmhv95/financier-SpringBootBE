d3af5e35e5b1d8eff51de4e8cb04749cab3e1429
bmhv95 <131663255+bmhv95@users.noreply.github.com>
2023-05-19 18:40:41 +0700

    Basic entities done, imports and mock data working

---
+++ src/main/java/com/personal/project/rest/GoalAPI.java
@@ -0,0 +1,25 @@
+package com.personal.project.rest;
+
+import com.personal.project.service.DTO.GoalDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/goals")
+public interface GoalAPI {
+    @PostMapping("/new")
+    ResponseEntity<GoalDTO> createNewGoal(GoalDTO goalDTO);
+
+    @GetMapping
+    ResponseEntity<List<GoalDTO>> getAllGoals();
+
+    @GetMapping("/{id}")
+    ResponseEntity<GoalDTO> getGoalById(Long id);
+
+    @PutMapping("/{id}")
+    ResponseEntity<GoalDTO> updateGoalById(Long id, GoalDTO goalDTO);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteGoalById(Long id);
+}
